49275ccefb8cb1dcdef493d3fc678ae8d16871da
kumokumo1653 <fjsda526hdbs@gmail.com>
2025-07-02 01:07:51 +0900

    l10nをProviderで提供

--- lib/l10n/l10n_provider.dart
+++ lib/l10n/l10n_provider.dart
@@ -1,21 +1,21 @@
 import 'dart:ui' as ui;
 
 import 'package:flutter/material.dart';
-import 'package:ideamaker/l10n/app_localizations.dart';
 import 'package:riverpod/riverpod.dart';
 
-final appLocalizationProvider = Provider<L10n>((ref) {
-  ref.state = lookupL10n(ui.window.locale); // デフォルトのL10n
+import 'app_localizations.dart';
+
+final l10nProvider = Provider<L10n>((ref) {
+  ref.state = lookupL10n(ui.PlatformDispatcher.instance.locale);
   final observer = _LocaleObserver((locales) {
-    ref.state = lookupL10n(ui.window.locale); // 設定した言語のL10nに更新する
+    ref.state = lookupL10n(ui.PlatformDispatcher.instance.locale);
   });
   final binding = WidgetsBinding.instance;
-  binding.addObserver(observer); // 言語切替検知の開始
+  binding.addObserver(observer);
   ref.onDispose(() => binding.removeObserver(observer));
   return ref.state;
 });
 
-/// 言語切替を検知するためのオブザーバー
 class _LocaleObserver extends WidgetsBindingObserver {
   _LocaleObserver(this._didChangeLocales);
 
