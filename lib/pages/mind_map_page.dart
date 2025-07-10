6284e8d1fe1763459a8d368cb37ec19bf5a06391
kumokumo1653 <fjsda526hdbs@gmail.com>
2025-07-11 00:52:07 +0900

    ページ修正

--- lib/pages/mind_map_page.dart
+++ lib/pages/mind_map_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:idea_maker/features/mind_map/widgets/mind_map_node.dart';
 
 class MindMapPage extends HookConsumerWidget {
   const MindMapPage({super.key});
