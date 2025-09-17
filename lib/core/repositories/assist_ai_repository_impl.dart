018808c58d1a0705aade7f23a61362a28587d5ac
kumokumo1653 <fjsda526hdbs@gmail.com>
2025-09-17 20:30:04 +0900

    GoogleAI使用時にAppcheckを利用するように修正

--- lib/core/repositories/assist_ai_repository_impl.dart
+++ lib/core/repositories/assist_ai_repository_impl.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:firebase_ai/firebase_ai.dart';
+import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:idea_maker/core/exceptions/assist_ai_exception.dart';
 import 'package:idea_maker/core/repositories/assist_ai_repository.dart';
 import 'package:idea_maker/utils/logger.dart';
@@ -8,9 +9,10 @@ import 'package:idea_maker/utils/logger.dart';
 class AssistAiRepositoryImpl implements AssistAIRepository {
   @override
   Future<List<String>> generateAssociations(String keyword) async {
-    final model = FirebaseAI.googleAI().generativeModel(
-      model: 'gemini-2.5-flash',
-    );
+    final model = FirebaseAI.googleAI(appCheck: FirebaseAppCheck.instance)
+        .generativeModel(
+          model: 'gemini-2.5-flash',
+        );
     // ignore: lines_longer_than_80_chars
     final prompt = [
       Content.text(
